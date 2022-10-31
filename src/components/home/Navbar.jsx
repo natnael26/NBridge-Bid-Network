7581c6c02dfc0ba41a0bc1b4d629426af17fd122
natnael26 <natnaeldessalegn26@gmail.com>
2022-10-31 17:00:47 +0300

    third

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 import '../../index.css';
 function Navbar() {
   return (
-    <nav className="navbar  navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg  navbar-light bg-light">
     <Link className="navbar-brand">NBridge Bid Network</Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
